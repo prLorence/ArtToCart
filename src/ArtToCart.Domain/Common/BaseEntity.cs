354cace74c85d7b502c4a152dfcff989ea1d6efe
prLorence <paulorencereyes@gmail.com>
2023-09-29 19:12:53 +0800

    fix(domain): base entity constructor

--- src/ArtToCart.Domain/Common/BaseEntity.cs
+++ src/ArtToCart.Domain/Common/BaseEntity.cs
@@ -1,12 +1,24 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
+using Microsoft.AspNetCore.Http;
+
 namespace ArtToCart.Domain.Common;
 
-public abstract class BaseEntity
+public abstract class BaseEntity<T>
+    where T : ValueObject
 {
-    public int Id { get; set; }
+    protected T Id { get; init; }
 
     private readonly List<BaseEvent> _domainEvents = new();
+
+    protected BaseEntity()
+    {
+
+    }
+    protected BaseEntity(T id)
+    {
+        Id = id;
+    }
 
     [NotMapped]
     public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
