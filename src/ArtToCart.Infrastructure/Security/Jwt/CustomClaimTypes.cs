2c1c4fdd093e9bc97c3d4d4d9442001b9f359070
prLorence <paulorencereyes@gmail.com>
2023-10-28 19:04:57 +0800

    feat(security): custom claim types

---
+++ src/ArtToCart.Infrastructure/Security/Jwt/CustomClaimTypes.cs
@@ -0,0 +1,7 @@
+namespace ArtToCart.Infrastructure.Shared.Jwt;
+
+public static class CustomClaimTypes
+{
+    public const string Permission = "permission";
+    public const string RefreshToken = "refreshToken";
+}
