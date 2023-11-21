80b742e04e99856d798cc765bb8e7a228410854e
prLorence <paulorencereyes@gmail.com>
2023-11-21 13:56:03 +0800

    feat(infra): entity configurations

---
+++ src/ArtToCart.Infrastructure/Data/EntityConfigurations/Products/CatalogItemConfiguration.cs
@@ -0,0 +1,49 @@
+using ArtToCart.Domain.Products;
+using ArtToCart.Domain.Products.ValueObjects;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ArtToCart.Infrastructure.Data.EntityConfigurations.Products;
+
+public class CatalogItemConfiguration : IEntityTypeConfiguration<CatalogItem>
+{
+    public void Configure(EntityTypeBuilder<CatalogItem> builder)
+    {
+        builder.ToTable("CatalogItems");
+
+        builder.HasKey(ci => ci.Id);
+
+        builder.Property(ci => ci.Id)
+            .HasConversion(id => id.Value,
+                value => CatalogItemId.CreateFrom(value));
+
+        builder.Property(ci => ci.Name).HasMaxLength(50);
+
+        builder.Property(ci => ci.Price);
+
+        builder.Property(ci => ci.Size);
+
+        builder.Property(ci => ci.Description).HasMaxLength(100);
+
+        builder.Property(ci => ci.SellerId);
+
+        builder.Property(ci => ci.CatalogTypeId)
+            .HasConversion(id => id.Value,
+                value => CatalogTypeId.CreateFrom(value));
+
+        builder.HasMany(ci => ci.Images)
+            .WithOne(i => i.CatalogItem)
+            .HasForeignKey(x => x.CatalogItemId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(ci => ci.CatalogType)
+            .WithMany()
+            .HasForeignKey(ci => ci.CatalogTypeId);
+
+        builder.Metadata
+            .FindNavigation(nameof(CatalogItem.Images))
+            .SetPropertyAccessMode(PropertyAccessMode.Field);
+
+    }
+}
---
+++ src/ArtToCart.Infrastructure/Data/EntityConfigurations/Products/ProductImageConfiguration.cs
@@ -0,0 +1,24 @@
+using ArtToCart.Domain.Products.Entities;
+using ArtToCart.Domain.Products.ValueObjects;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ArtToCart.Infrastructure.Data.EntityConfigurations.Products;
+
+public class ProductImageConfiguration : IEntityTypeConfiguration<ProductImage>
+{
+    public void Configure(EntityTypeBuilder<ProductImage> builder)
+    {
+        builder.ToTable("product_images");
+
+        builder.HasKey(c => c.Id);
+
+        builder.HasIndex(x => x.Id).IsUnique();
+
+        builder.Property(x => x.Id)
+            .HasConversion(id => id.Value,
+                value=> ProductImageId.CreateFrom(value))
+            .ValueGeneratedNever();
+    }
+}
