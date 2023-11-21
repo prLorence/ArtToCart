0a8a96265798892b5338391b28defe2e9050ea54
prLorence <paulorencereyes@gmail.com>
2023-11-21 13:48:49 +0800

    fix(refactor): file moves

--- src/ArtToCart.Infrastructure/Data/EntityConfigurations/Identity/RefreshTokenConfiguration.cs
+++ src/ArtToCart.Infrastructure/Data/EntityConfigurations/Identity/RefreshTokenConfiguration.cs
@@ -3,7 +3,7 @@ using ArtToCart.Application.Shared.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace ArtToCart.Infrastructure.Data.EntityConfigurations;
+namespace ArtToCart.Infrastructure.Data.EntityConfigurations.Identity;
 
 public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
 {
