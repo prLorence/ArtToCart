4b919761a9cee2692578125d06bc349e8c6a8c12
prLorence <paulorencereyes@gmail.com>
2023-11-26 17:37:29 +0800

    feat(infra): added artists role sql reference

--- src/ArtToCart.Application/Shared/Models/ApplicationRole.cs
+++ src/ArtToCart.Application/Shared/Models/ApplicationRole.cs
@@ -9,11 +9,21 @@ namespace ArtToCart.Application.Shared.Models;
 public class ApplicationRole : IdentityRole<Guid>
 {
     private static readonly Guid ADMIN_ID = Guid.Parse("2c5e174e-3b0e-446f-86af-483d56fd7210");
+    private static readonly Guid ARTIST_ID = Guid.Parse("b5931487-0f74-421e-8194-ebd2afddd69a");
     private static readonly Guid USER_ID = Guid.Parse("a18be9c0-aa65-4af8-bd17-00bd9344e575");
+
+    public static ApplicationRole Artist => new()
+    {
+        Id = ARTIST_ID,
+        Name = Constants.Role.Artist,
+        NormalizedName = nameof(Artist).ToUpper(CultureInfo.InvariantCulture),
+    };
 
     public static ApplicationRole User => new()
     {
-        Id = ADMIN_ID, Name = Constants.Role.User, NormalizedName = nameof(User).ToUpper(CultureInfo.InvariantCulture),
+        Id = ADMIN_ID,
+        Name = Constants.Role.User,
+        NormalizedName = nameof(User).ToUpper(CultureInfo.InvariantCulture),
     };
 
     public static ApplicationRole Admin => new()
