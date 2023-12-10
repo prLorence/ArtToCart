d078df5aa7b31160ebb70ffd62e64bd44f2b84de
prLorence <paulorencereyes@gmail.com>
2023-12-10 14:25:57 +0800

    chore: map config for basket item id to dto

--- src/ArtToCart.Application/Baskets/Shared/BasketMapConfig.cs
+++ src/ArtToCart.Application/Baskets/Shared/BasketMapConfig.cs
@@ -11,12 +11,13 @@ public class BasketMapConfig : IRegister
      public void Register(TypeAdapterConfig config)
      {
          config.NewConfig<Basket, BasketDto>()
+             .Map(dest => dest.BasketId, src => src.Id.Value.ToString())
              .Map(dest => dest.BuyerId, src => src.BuyerId)
              .Map(dest => dest.Items, src => src.Items);
 
          config.NewConfig<BasketItem, BasketItemDto>()
+             .Map(dest => dest.Id, src => src.Id.Value.ToString())
              .Map(dest => dest.CatalogItemId, src => src.CatalogItemId)
-             .Map(dest => dest.BasketId, src => src.BasketId.Value.ToString())
-             .Ignore(dest => dest.Basket, src=> src.Basket);
+             .Map(dest => dest.BasketId, src => src.BasketId.Value.ToString());
      }
 }
