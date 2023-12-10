8d2a51232d80cf20c432fe6bb352bad0856f89ba
prLorence <paulorencereyes@gmail.com>
2023-12-10 14:25:05 +0800

    feat: update basket item dto

--- src/ArtToCart.Application/Baskets/Shared/BasketItemDto.cs
+++ src/ArtToCart.Application/Baskets/Shared/BasketItemDto.cs
@@ -2,9 +2,15 @@ namespace ArtToCart.Application.Baskets.Shared;
 
 public class BasketItemDto
 {
+    public string Id { get; set; }
     public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
     public string CatalogItemId { get; set; }
     public string BasketId { get; set; }
-    public string Basket { get; set; }
+}
+
+public class UpdateBasketItemDto
+{
+    public string Id { get; set; }
+    public int Quantity { get; set; }
 }
