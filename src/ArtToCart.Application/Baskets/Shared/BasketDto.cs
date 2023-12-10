e606b74b44afc524d60dc0cfb46c39ae5191796f
prLorence <paulorencereyes@gmail.com>
2023-12-10 14:24:40 +0800

    fix(application): additional id field for basket dto

--- src/ArtToCart.Application/Baskets/Shared/BasketDto.cs
+++ src/ArtToCart.Application/Baskets/Shared/BasketDto.cs
@@ -4,6 +4,7 @@ namespace ArtToCart.Application.Baskets.Shared;
 
 public class BasketDto
 {
+    public string BasketId { get; set; }
     public string BuyerId { get; set; }
     public List<BasketItemDto> Items { get; set; }
     public int TotalItemCount;
