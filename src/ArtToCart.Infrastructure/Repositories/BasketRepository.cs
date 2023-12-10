93add9ea24bbc644481bd7c4f9933a7355e8978c
prLorence <paulorencereyes@gmail.com>
2023-12-10 14:25:38 +0800

    feat(infrastructure): conditional find for basket

--- src/ArtToCart.Infrastructure/Repositories/BasketRepository.cs
+++ src/ArtToCart.Infrastructure/Repositories/BasketRepository.cs
@@ -1,5 +1,6 @@
 using ArtToCart.Application.Shared.Interfaces;
 using ArtToCart.Domain.Baskets;
+using ArtToCart.Domain.Baskets.ValueObjects;
 using ArtToCart.Infrastructure.Shared;
 
 using Microsoft.EntityFrameworkCore;
@@ -27,9 +28,12 @@ public class BasketRepository : IRepository<Basket>
     public async Task<Basket> FirstOrDefaultAsync(string id)
     {
        var userItems = await _context.Basket
-            .Where(b => b.BuyerId == id)
-            .Include(b => b.Items)
-            .FirstOrDefaultAsync();
+           .Where(b => b.BuyerId == id)
+           .Include(b => b.Items)
+           .FirstOrDefaultAsync() ?? await _context.Basket
+               .Where(b => b.Id == BasketId.CreateFrom(id))
+               .Include(b => b.Items)
+               .FirstOrDefaultAsync();
 
        return userItems;
     }
