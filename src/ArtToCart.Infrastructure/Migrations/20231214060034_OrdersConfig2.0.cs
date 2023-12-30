c2dc5e74a1c1e66fb1bd405256d64e7bdafb39bc
prLorence <paulorencereyes@gmail.com>
2023-12-30 13:40:31 +0800

    feat(infra): mvp domain model ef config

---
+++ src/ArtToCart.Infrastructure/Migrations/20231214060034_OrdersConfig2.0.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtToCart.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrdersConfig20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShipToAddress_State",
+                table: "Order",
+                newName: "ShipToAddress_Province");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShipToAddress_Province",
+                table: "Order",
+                newName: "ShipToAddress_State");
+        }
+    }
+}
---
+++ src/ArtToCart.Infrastructure/Migrations/20231218104230_BasketItemSize.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtToCart.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class BasketItemSize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "BasketItem",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "BasketItem");
+        }
+    }
+}
