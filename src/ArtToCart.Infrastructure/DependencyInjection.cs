bc439d19af5460b08ce479a3316575bd29a0497a
prLorence <paulorencereyes@gmail.com>
2024-01-19 16:21:30 +0800

    chore: trying to access swagger ui

--- src/ArtToCart.Infrastructure/DependencyInjection.cs
+++ src/ArtToCart.Infrastructure/DependencyInjection.cs
@@ -39,7 +39,7 @@ public static class DependencyInjection
 
         string connectionString = config.GetConnectionString("ArtToCartPsqlConnection");;
 
-        if (environment.IsProduction())
+        if (!environment.IsProduction())
         {
             SecretClientOptions options = new()
             {
